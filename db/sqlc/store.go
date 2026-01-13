ac61e80075878ab1aead76835502cac4eb94a739
Michael <michael.esenwa@gmail.com>
2026-01-13 10:28:58 +0100

    Add mock store implementation and update SQL store interface
    - Introduced a new mock store for testing purposes using MockGen.
    - Updated the Store type to an interface and created SQLStore struct to implement it.
    - Modified the NewServer function to accept the Store interface directly.
    - Enabled interface generation in sqlc.yaml for better integration with mock testing.
    - Added a Makefile target for generating mocks.

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -8,20 +8,25 @@ import (
 )
 
 // Provides all functions to execute db queries and functions
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+type SQLStore struct {
 	*Queries
 	db *pgxpool.Pool
 }
 
-func NewStore(db *pgxpool.Pool) *Store {
-	return &Store{
+func NewStore(db *pgxpool.Pool) *SQLStore {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // ExecTx executes a function within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.Begin(ctx)
 	if err != nil {
 		return err
@@ -57,7 +62,7 @@ type TransferTxResult struct {
 
 // TransferTx performs a money transfer from one account to the other.
 // It creates the transfer, add account entries, and update accounts' balance within a database transaction
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
